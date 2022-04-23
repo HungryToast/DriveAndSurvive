45a5bdf19461ba0b67700e2a638b2e783c6c16e8
Abdullah <ahiyari99@gmail.com>
2022-04-23 02:19:07 +0100

    car

--- DNS/Assets/Scripts/CarController.cs
+++ DNS/Assets/Scripts/CarController.cs
@@ -7,17 +7,40 @@ using UnityEngine.InputSystem;
 public class CarController : MonoBehaviour
 {
     private InputAction drive;
+    [SerializeField]private WheelCollider[] _wheelColliders;
+    [SerializeField] private float driveForce;
+    [SerializeField] private float brakeForce;
+    [SerializeField] private bool parking;
 
     private void OnEnable()
     {
+        //input map change when exiting vehichle
         drive = InputManager.inputActions.Car.Drive;
-        InputManager.inputActions.Car.Exit.performed += ExitTractor;
+        InputManager.inputActions.Car.Exit.performed += ExitCar;
+        
+        
     }
 
-    private void ExitTractor(InputAction.CallbackContext obj)
+    private void FixedUpdate()
+    {
+        
+        
+        if (drive.ReadValue<Vector2>().sqrMagnitude > 0.1f )
+        {
+            for (int i = 0; i < _wheelColliders.Length; i++)
+            {
+                _wheelColliders[i].motorTorque = driveForce;
+            }
+        }
+        
+
+        
+    }
+
+    
+    private void ExitCar(InputAction.CallbackContext obj)
     {
         InputManager.ToggleActionMap(InputManager.inputActions.Player);
     }
-    
-    
+
 }
